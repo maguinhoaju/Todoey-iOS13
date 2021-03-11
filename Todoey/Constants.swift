eb0976e69b0d8221fca651434a3f828a2bc90f9e
Maguinho <maguinho.aju@gmail.com>
2021-03-11 12:22:34 -0300

    Alterações

--- Todoey/Constants.swift
+++ Todoey/Constants.swift
@@ -3,7 +3,6 @@
 //  Todoey
 //
 //  Created by Carlos Magno de França Veiga on 11/03/21.
-//  Copyright © 2021 App Brewery. All rights reserved.
 //
 
 import Foundation
--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["FInd Mike", "Buy Eggs", "Destroy Demogorfon"]
+    var itemArray = ["FInd Mike", "Buy Eggs", "Destroy Demogorfon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,11 +42,28 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
-        
-        
         //retira a seleção da linha quando clicada
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    
+    //MARK: - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //o que acontecerá quando o usuário clicar no botão Add Item on UIAlert
+            self.itemArray.append(textField.text!)
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
